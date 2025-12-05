fc392e250018e8af6b5610272e15b5697fbc8958
gaja-123 <gajarajan23@gmail.com>
2025-12-05 11:35:55 +0530

    define variables in typescript

---
+++ TypeScript-train/02- variable/sampletype.js
@@ -0,0 +1,8 @@
+var found = true;
+var grade = 98.5;
+var firstName = 'ram';
+var secondName = 'krishna';
+console.log(found);
+console.log(grade);
+console.log(firstName);
+console.log("Hi ".concat(firstName, " ").concat(secondName));
---
+++ TypeScript-train/02- variable/sampletype.ts
@@ -0,0 +1,9 @@
+let found : boolean = true
+let grade : number = 98.5
+let firstName : string ='ram'
+let secondName : string ='krishna'
+
+console.log(found)
+console.log(grade)
+console.log(firstName)
+console.log(`Hi ${firstName} ${secondName}`)
