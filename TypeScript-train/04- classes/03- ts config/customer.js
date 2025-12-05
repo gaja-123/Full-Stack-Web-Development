62d6b83024ec0c57527b895bb79f7ba18d55c337
gaja-123 <gajarajan23@gmail.com>
2025-12-05 15:04:41 +0530

    typescript tsconfig.json file used for complier option setting onetime

---
+++ TypeScript-train/04- classes/03- ts config/customer.js
@@ -0,0 +1,33 @@
+"use strict";
+var customer = /** @class */ (function () {
+    function customer(fname, lname) {
+        this._firstName = fname;
+        this._lastName = lname;
+    }
+    Object.defineProperty(customer.prototype, "firstName", {
+        get: function () {
+            return this._firstName;
+        },
+        set: function (value) {
+            this._firstName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(customer.prototype, "lastName", {
+        get: function () {
+            return this._lastName;
+        },
+        set: function (value) {
+            this._lastName = value;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return customer;
+}());
+var mycus = new customer('rao', 'srinu');
+// mycus.firstName='ram'
+// mycus.lastName='ramesh'
+mycus.firstName = 'lord';
+console.log(mycus.firstName + ' ' + mycus.lastName);
