f0927a8972db1e365e2ca855dffaa4972d22cd8e
gaja-123 <gajarajan23@gmail.com>
2025-12-05 11:38:01 +0530

    define variables in typescript

--- TypeScript-train/02- variable/sampletype.ts
+++ TypeScript-train/02- variable/sampletype.ts
@@ -3,7 +3,14 @@ let grade : number = 98.5
 let firstName : string ='ram'
 let secondName : string ='krishna'
 
+
+found=false
+grade=100 
+
+
+//found=100 
+
 console.log(found)
 console.log(grade)
 console.log(firstName)
-console.log(`Hi ${firstName} ${secondName}`)
+console.log(`Hi ${firstName} ${secondName}`) 
