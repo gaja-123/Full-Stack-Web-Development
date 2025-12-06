414ee06642cbe23a6d757790d5ab65afa685f2f2
gaja-123 <gajarajan23@gmail.com>
2025-12-06 08:46:51 +0530

    TypeScript : inheritance with arary of class and ploymorphism of method

---
+++ TypeScript-train/05 - inheritance/arraydriver.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Shape_1 = require("./Shape");
+var Circle_1 = require("./Circle");
+var Rectangle_1 = require("./Rectangle");
+var s1 = new Shape_1.Shape(13, 12);
+var c1 = new Circle_1.Circle(11, 21, 31);
+var r1 = new Rectangle_1.Rectangle(1, 2, 3, 4);
+var as = [];
+as.push(s1);
+as.push(c1);
+as.push(r1);
+//as.push(1)
+for (var _i = 0, as_1 = as; _i < as_1.length; _i++) {
+    var ele = as_1[_i];
+    console.log(ele.getinfo());
+}
