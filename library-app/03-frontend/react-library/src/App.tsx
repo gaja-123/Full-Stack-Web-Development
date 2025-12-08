820bc2a7840443c5fbe579785a4e30967732e66a
gaja-123 <gajarajan23@gmail.com>
2025-12-08 11:36:13 +0530

    added react frontend : hero banner component

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import { NavBar } from './layouts/navbar-and-footer/NavBar';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <ExploreTopBooks/>
     </div>
   );
 }
