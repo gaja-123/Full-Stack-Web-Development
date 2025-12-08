a35365660c3552064681fce22163d596a9850f88
gaja-123 <gajarajan23@gmail.com>
2025-12-08 09:44:42 +0530

    setup for library project done - starterfiles,frantend,backend(empty)

---
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Welcome to new journey of starting a Full Stack Webdevelopment library project </h1>
+    </div>
+  );
+}
+
+export default App;
