43e1e7f1cd045905743036b93be74928ad94fa96
gaja-123 <gajarajan23@gmail.com>
2025-12-08 16:10:31 +0530

    added react frontend: homepage component for clearer App.js

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -1,20 +1,13 @@
-import React from 'react';
 import './App.css';
 import { NavBar } from './layouts/navbar-and-footer/NavBar';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/HomePage/Carousel';
-import { Heros } from './layouts/HomePage/Heros';
-import { LibraryServices } from './layouts/HomePage/LibraryServices';
 import { Footer } from './layouts/navbar-and-footer/Footer';
+import { HomePage } from './layouts/HomePage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <ExploreTopBooks/>
-      <Carousel/>
-      <Heros/>
-      <LibraryServices/>
+      <HomePage/>
       <Footer/>
     </div>
   );
