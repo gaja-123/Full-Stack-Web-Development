dc0abc847ce30d417088378569fde27c8cd1aaff
gaja-123 <gajarajan23@gmail.com>
2025-12-08 12:12:36 +0530

    added react frontend: carousel component for top books but for mobile only one book shows

--- library-app/03-frontend/react-library/src/App.tsx
+++ library-app/03-frontend/react-library/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { NavBar } from './layouts/navbar-and-footer/NavBar';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
       <ExploreTopBooks/>
+      <Carousel/>
     </div>
   );
 }
